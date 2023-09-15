35f3cfb4b43742fc558cbffe0d05e033bd4fe59f
Tailwind Mechanics <shea@tailwindmechanics.com>
2023-09-15 22:39:56 +0100

    Procedural hex now composable

--- Assets/_Modules/HexTiles/Scripts/Internal/Behaviour/HexRenderer.cs
+++ Assets/_Modules/HexTiles/Scripts/Internal/Behaviour/HexRenderer.cs
@@ -2,24 +2,49 @@ using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using System.Linq;
 using UnityEngine;
-
+using System;
+using JetBrains.Annotations;
 using Modules.HexTiles.Internal.DataObjects;
 
 
 namespace Modules.HexTiles.Internal.Behaviour
 {
+    [Serializable]
+    public class EdgeLoop
+    {
+        public float InnerRadius => innerRadius;
+        public float InnerHeight => innerHeight;
+        public float OuterRadius => outerRadius;
+        public float OuterHeight => outerHeight;
+        public bool Reverse => reverse;
+
+        [FoldoutGroup("$GroupName"), SerializeField]
+        string label = "Untitled";
+
+        [FoldoutGroup("$GroupName"), Range(0f, 1f), SerializeField]
+        float innerRadius = .9f;
+        [FoldoutGroup("$GroupName"), Range(0f, 1f), SerializeField]
+        float outerRadius = .9f;
+
+        [FoldoutGroup("$GroupName"), Range(0f, 1f), SerializeField]
+        float innerHeight = 1f;
+        [FoldoutGroup("$GroupName"), Range(0f, 1f), SerializeField]
+        float outerHeight = 1f;
+        [FoldoutGroup("$GroupName"), SerializeField]
+        bool reverse;
+
+        [UsedImplicitly]
+        string GroupName => string.IsNullOrEmpty(label) ? "Untitled" : label;
+    }
+
     [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
     public class HexRenderer : MonoBehaviour
     {
         [SerializeField]
         Material material;
 
-        [Range(0f, 2f), OnValueChanged(nameof(OnChange)), SerializeField]
-        float inner = 0.5f;
-        [Range(0f, 2f), OnValueChanged(nameof(OnChange)), SerializeField]
-        float outer = 1f;
-        [Range(0f, 2f), OnValueChanged(nameof(OnChange)), SerializeField]
-        float height = 1f;
+        [SerializeField] List<EdgeLoop> edgeLoops = new();
+
 
         MeshRenderer meshRenderer;
         MeshFilter meshFilter;
@@ -31,58 +56,51 @@ namespace Modules.HexTiles.Internal.Behaviour
             meshFilter = GetComponent<MeshFilter>();
         }
 
-        void Start() => DrawMesh();
-        void OnChange () => DrawMesh();
-
-        void DrawMesh()
+        [Button(ButtonSizes.Medium)]
+        void DrawMesh2 ()
         {
-            if (!Application.isPlaying) return;
+            var loops = new List<Face>();
+            edgeLoops.ForEach(loop =>
+            {
+                var faces = CreateLoop(loop);
+                loops.AddRange(faces);
+            });
 
-            var faces = DrawFaces(inner, outer, height);
-            meshFilter.mesh = CombineFaces(faces);
+            meshFilter.mesh = CombineFaces(loops);
             meshRenderer.material = material;
         }
 
-        List<Face> DrawFaces (float innerSize, float outerSize, float newHeight)
+        List<Face> CreateLoop (EdgeLoop edgeLoop)
         {
             var result = new List<Face>();
-            var halfHeight = newHeight / 2f;
-
-            var topFaces = DrawFaces(innerSize, outerSize, halfHeight, halfHeight);
-            var bottomFaces = DrawFaces(innerSize, outerSize, -halfHeight, -halfHeight, true);
-            var outerFaces = DrawFaces(outerSize, outerSize, halfHeight, -halfHeight, true);
-            var innerFaces = DrawFaces(innerSize, innerSize, halfHeight, -halfHeight);
-
-            result.AddRange(topFaces);
-            result.AddRange(bottomFaces);
-            result.AddRange(outerFaces);
-            result.AddRange(innerFaces);
+            var faces = CreateFaces(edgeLoop);
+            result.AddRange(faces);
 
             return result;
         }
 
-        IEnumerable<Face> DrawFaces (float innerSize, float outerSize, float heightA, float heightB, bool reverse = false)
+        IEnumerable<Face> CreateFaces (EdgeLoop edgeLoop)
         {
             var result = new List<Face>();
             for (var point = 0; point < 6; point++)
             {
-                result.Add(CreateFace(innerSize, outerSize, heightA, heightB, point, reverse));
+                result.Add(CreateFace(edgeLoop, point));
             }
 
             return result;
         }
 
-        Face CreateFace (float innerRadius, float outerRadius, float heightA, float heightB, int point, bool reverse = false)
+        Face CreateFace (EdgeLoop edgeLoop, int point)
         {
-            var pointA = GetPoint(innerRadius, heightB, point);
-            var pointB = GetPoint(innerRadius, heightB, point < 5 ? point + 1 : 0);
-            var pointC = GetPoint(outerRadius, heightA, point < 5 ? point + 1 : 0);
-            var pointD = GetPoint(outerRadius, heightA, point);
+            var pointA = GetPoint(edgeLoop.InnerRadius, edgeLoop.InnerHeight, point);
+            var pointB = GetPoint(edgeLoop.InnerRadius, edgeLoop.InnerHeight, point < 5 ? point + 1 : 0);
+            var pointC = GetPoint(edgeLoop.OuterRadius, edgeLoop.OuterHeight, point < 5 ? point + 1 : 0);
+            var pointD = GetPoint(edgeLoop.OuterRadius, edgeLoop.OuterHeight, point);
 
             var vertices = new List<Vector3> { pointA, pointB, pointC, pointD };
             var triangles = new List<int> { 0, 1, 2, 2, 3, 0 };
             var uvs = new List<Vector2> { Vector2.zero, Vector2.right, Vector2.one, Vector2.up };
-            if (reverse) vertices.Reverse();
+            if (edgeLoop.Reverse) vertices.Reverse();
 
             return new Face(vertices, triangles, uvs);
         }
