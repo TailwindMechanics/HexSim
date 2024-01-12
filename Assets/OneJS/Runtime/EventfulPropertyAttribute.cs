3545772a0ea1f1acf07b10a4abe10297c9d3e583
Tailwind Mechanics <shea@tailwindmechanics.com>
2024-01-12 09:46:50 +0000

    Removed OneJs

--- Assets/OneJS/Runtime/EventfulPropertyAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace OneJS {
-    /// <summary>
-    /// Generate additional property and event that are compatible with useEventfulState
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
-    public class EventfulPropertyAttribute : Attribute { }
-}
